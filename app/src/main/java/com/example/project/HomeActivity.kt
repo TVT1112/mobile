8aa56ad72abf26a9e6c7649c40d127c36b179373
THAIVANTUAN <thait2710@gmail.com>
2025-05-09 00:19:40 +0700

    next

--- app/src/main/java/com/example/project/HomeActivity.kt
+++ app/src/main/java/com/example/project/HomeActivity.kt
@@ -1,8 +1,13 @@
 package com.example.project
 
 import android.os.Bundle
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,10 +16,33 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val sharedPreferences = getSharedPreferences("shared_prefs", MODE_PRIVATE)
+        val username = sharedPreferences.getString("username","").toString();
+        Toast.makeText(applicationContext, "Chào mừng "+username, Toast.LENGTH_SHORT).show();
+
+        val exit: CardView = findViewById(R.id.cardExit)
+        exit.setOnClickListener {
+            val editor = sharedPreferences.edit()
+            editor.clear()
+            editor.apply() // Lưu thay đổi
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent);
+
+            Toast.makeText(this, "Đăng xuất thành công", Toast.LENGTH_SHORT).show()
+            finish() // Kết thúc activity (có thể chuyển về màn login nếu cần)
+        }
+
+        val findDoctor: CardView = findViewById(R.id.cardFindDoctor)
+        findDoctor.setOnClickListener{
+            val intent = Intent(this, FindDoctorActivity::class.java)
+            startActivity(intent);
         }
     }
 }
